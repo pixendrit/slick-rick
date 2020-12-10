dcc8aa8336fad9209cf339e92d8ef3191b26cf01
pixendrit <pixendrit@outlook.com>
2020-12-10 13:51:54 +0100

    first commit

---
+++ master-gatsby-master/starter-files/gatsby/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function HomePage() {
+  return (
+    <>
+      <p>Hey!I'm the homepage!</p>
+      <p>Hey!I'm the another element!</p>
+    </>
+  );
+}
---
+++ master-gatsby-master/starter-files/gatsby/src/pages/slicemaster.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+export default function SliceMasterPage() {
+  return (
+    <>
+      <p>Hey!I'm the SliceMasters Page!</p>
+    </>
+  );
+}
